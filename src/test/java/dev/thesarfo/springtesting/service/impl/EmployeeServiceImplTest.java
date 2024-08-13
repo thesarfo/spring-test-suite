b8f554df19d330c510e3ff41b6893dbea6c8ef79
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-08-13 19:47:37 +0000

    test method that throws exception

--- src/test/java/dev/thesarfo/springtesting/service/impl/EmployeeServiceImplTest.java
+++ src/test/java/dev/thesarfo/springtesting/service/impl/EmployeeServiceImplTest.java
@@ -1,7 +1,9 @@
 package dev.thesarfo.springtesting.service.impl;
 
+import dev.thesarfo.springtesting.exception.ResourceNotFoundException;
 import dev.thesarfo.springtesting.model.Employee;
 import dev.thesarfo.springtesting.repository.EmployeeRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,7 +15,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class EmployeeServiceImplTest {
@@ -49,5 +54,21 @@ class EmployeeServiceImplTest {
 
         // then - verify the output
         assertThat(savedEmployee).isNotNull();
+    }
+
+    @DisplayName("Unit test for saveEmployee method which throws exception")
+    @Test
+    public void givenExistingEmail_whenSaveEmployee_thenThrowsException(){
+        // given - precondition or setup
+        given(employeeRepository.findByEmail(employee.getEmail()))
+                .willReturn(Optional.of(employee));
+//        given(employeeRepository.save(employee)).willReturn(employee);
+
+        // when - action or behaviour to be tested
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> employeeService.saveEmployee(employee));
+
+        // then - verify the output
+        //assertThat(savedEmployee).isNotNull();
+        verify(employeeRepository, never()).save(any(Employee.class));
     }
 }
